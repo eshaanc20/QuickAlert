d68aee3117dc04d8c2df2ada3f43d0ad51e29970
Rupinder Nagra <rupinder0410@gmail.com>
2020-01-25 15:50:03 -0500

    added title component and path

--- src/Components/IntroPage/Homepage.js
+++ src/Components/IntroPage/Homepage.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar'
-
+import Title from './Title'
 
 class Homepage extends Component {
     render () {
         return (
-            <div>
+            <div style={{ width: "100vw", height: "100vh", backgroundColor: "#ff5050"}}>
                 <Navbar />
+                <Title />
             </div>
-
-
-
         )
     }
 }
