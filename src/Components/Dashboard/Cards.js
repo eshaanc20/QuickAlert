63072fe26557f2e05f6420e5c7b1cb3cb238ef56
Rupinder Nagra <rupinder0410@gmail.com>
2020-01-26 08:06:55 -0500

    Improved cards for dashboard

--- src/Components/Dashboard/Cards.js
+++ src/Components/Dashboard/Cards.js
@@ -3,32 +3,38 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import './Cards.css';
 
 class Cards extends Component {
-
+    responded = () => {
+        axios.post("https://quick-alert.herokuapp.com/responded", {
+            name: this.props.name
+        })
+    }
     render () {
         return (
-            <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", marginTop: "10%"}}>
-                <Card style={{width: "20%", height: "40%"}}>
+            <div style={{width: '25%'}}>
+                <Card className="element">
                     <CardActionArea>
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="h2">
                                 {this.props.name}
                             </Typography>
                             <Typography variant="body2" color="textSecondary" component="p">
-                                <p>Location: {this.props.address}</p>
-                                <p>Phone Number: {this.props.phoneNumber}</p>
-                                <p>Age: {this.props.age}</p>
-                                <p>Medical Conditions: {this.props.conditions}</p>
-                                <p>Other Details: {this.props.otherDetails}</p>
+                                <p><strong>Location:</strong> {this.props.address}</p>
+                                <p><strong>Phone Number:</strong> {this.props.phoneNumber}</p>
+                                <p><strong>Age:</strong> {this.props.age}</p>
+                                <p><strong>Medical Conditions:</strong> {this.props.medicalConditions}</p>
+                                <p><strong>Other Details:</strong> {this.props.otherDetails}</p>
+                                <p><strong>Time:</strong> {this.props.time.substring(0,24)}</p>
                             </Typography>
                         </CardContent>
                     </CardActionArea>
                     <CardActions>
-                        <Button size="small" style={{color: "#f44336"}}>
+                        <Button size="small" style={{color: "#f44336", marginLeft: '40%'}} onClick={this.responded}>
                             Respond
                         </Button>
                     </CardActions>
