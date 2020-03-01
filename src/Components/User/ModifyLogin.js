9d3c0bfc638c0fd2533150f9b9bb758943379d96
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-02-29 20:37:50 -0500

    Added component for modifying login information

--- src/Components/User/ModifyLogin.js
+++ src/Components/User/ModifyLogin.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
+import './User.css';
 
 class Information extends Component {
     render() {
         return (
-            <div style={{display: 'flex', flexDirection: 'column', width: '40%', maxWidth: '400px', margin: 'auto'}}>
+            <div className='loginInfo-container'>
                 <TextField
                     label='Name'
                     defaultValue={this.props.info.name}
+                    variant='outlined'
+                />
+                <TextField
+                    label='Phone Number'
+                    defaultValue={this.props.info.phoneNumber}
+                    variant='outlined'
                 />
                 <TextField
                     label='Email'
                     defaultValue={this.props.info.email}
+                    variant='outlined'
                 />
                 <TextField
                     label='Password'
                     type='password'
                     defaultValue={this.props.info.password}
+                    variant='outlined'
+                />
+                <TextField
+                    label='Age'
+                    defaultValue={this.props.info.age}
+                    variant='outlined'
                 />
             </div>
         )
